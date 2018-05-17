df6531c126353a244d8724e8ca93cfacbef44efb
Ratnasambhav Priyadarshi <ratnasambhav@montaigne.co>
2018-05-17 12:41:33 +0530

    Added navbar

--- public/javascripts/delicious-app.js
+++ public/javascripts/delicious-app.js
@@ -1,3 +1,14 @@
 import '../sass/style.scss';
 
 import { $, $$ } from './modules/bling';
+
+$('#nav').addEventListener('click', navbar)
+
+function navbar() {
+  const x = $('#nav');
+  if (x.className === 'nav') {
+      x.className += ' responsive';
+  } else {
+      x.className = 'nav';
+  }
+}
