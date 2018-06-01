02c67c86cf60250c5d12ae9cdc96dc93ddf41c4d
Ratnasambhav Priyadarshi <ratnasambhav@montaigne.co>
2018-06-01 19:03:18 +0530

    Launch Day Debugging XD

--- routes/index.js
+++ routes/index.js
@@ -5,27 +5,51 @@ const newsletterSubscriberController = require('../controllers/newsletterSubscri
 const applicantController = require('../controllers/applicantController')
 
 router.get('/', (req, res) => {
-  res.render('index', {title: 'Welcome'});
+  res.render('index', {
+    title: 'Welcome',
+    description: 'Montaigne smart business solutions is a startup India recognized, Hyderabad based startup which offers smart solutions to eliminate complications during any stage of business growth. Montaigne can incubate various kinds of business with their diverse knowledge of strategies and ideas to get your position to top among your competitors.',
+    keywords: 'Business, Strategy, Incubator, Business growth, Business solutions, Smart business solutions, Data analysis, Predictive analytics'
+  });
 });
 
 router.get('/skillset', (req, res) => {
-  res.render('skillset', {title: 'Skillset'});
+  res.render('skillset', {
+    title: 'Skillset',
+    description: 'Montaigne is well known for their multiple skillsets as they deal with diverse platforms to provide accurate results to your business. We can incubate your business with our unique products like predictive analytics tool along with creative designing team we can deliver outstanding results.',
+    keywords: 'Incubation, Incubator, Business Incubator, Business incubation, Business optimization, Creative designing, Product marketing'
+  });
 });
 
 router.get('/get_in_touch', (req, res) => {
-  res.render('get_in_touch', {title: 'Get In Touch'});
+  res.render('get_in_touch', {
+    title: 'Get In Touch',
+    description: 'Contact us to get stand alone business plans and strategies to boost your business to higher level. Talk to our experts to customize your requirements and to get optimal solutions.',
+    keywords: 'Contact Montaigne, Montaigne support, Montaigne Smart Business Solutions, Product development, Startup development'
+  });
 });
 
 router.get('/our_story', (req, res) => {
-  res.render('our_story', {title: 'Our Story'});
+  res.render('our_story', {
+    title: 'Our Story',
+    description: 'Our team are exceptional problem solvers who are trained and exposed to multiple complex situations where achieving top rank is their only motto. Our team can workout better solution to optimize your business at higher standards.',
+    keywords: 'Marketing strategist, Product engineer, Marketing team, Business optimization, Business strategies'
+  });
 });
 
 router.get('/blog', (req, res) => {
-  res.render('blog', {title: 'Blog'});
+  res.render('blog', {
+    title: 'Blog',
+    description: 'Stay updated with our latest insights and strategies for different industries. Find relevant business plans and strategies for your industry to get optimal solutions. Subscribe to our newsletters for daily updates.',
+    keywords: 'Latest market trends, Recent market insights, Business reports, business plans, Business strategy, Business development plan, Business development, Business insights, Business Updates'
+  });
 });
 
 router.get('/working_with_us', (req, res) => {
-  res.render('working_with_us', {title: 'Working With Us'});
+  res.render('working_with_us', {
+    title: 'Working With Us',
+    description: 'Any one who are complex enough to understand and broad enough to learn new things to deliver acclaimed results are grandly welcome to be part of us. Long term internships are provided with various new learnings at work.',
+    keywords: 'Join us, Marketing jobs, Data analysis jobs, Web development job, Internships at Hyderabad, Internships in Hyderabad, Internships, UI/UX jobs, Startup internships, Startup jobs'
+  });
 });
 
 router.post('/get_in_touch', getInTouchController.addToDB);
--- views/mixins/changeMember.js
+++ views/mixins/changeMember.js
@@ -1,169 +1,125 @@
 const memberDetails = [
     {
+        firstName: 'Ananya',
+        lastName: 'Chandra',
+        designation: 'Content Engineer',
+        details: 'A get-it-done policy individual running forward in life to learn and grow more. By the way,she binge watches shows, sings and looks for excellent buffet places around.',
+        social: [
+            {
+                icon: 'Li',
+                href: 'https://www.linkedin.com/in/ananya-chalakani-981842143/'
+            }
+        ]
+    },
+    {
+        firstName: 'Rohith',
+        lastName: 'Sampathi',
+        designation: 'CEO & Founder',
+        details: 'Rohith Sampathi is a management consultant, free-lance writer, business marketer and an automotive enthusiast. During his journey with market intelligence industry, he was extensively working on research modeling for animal nutrition, automotive and machinery industries. Further, having worked as the Marketing Lead for a market research firm in the highly competitive paid marketing arena, his expertise in marketing for a diverse user-base is notable.',
+        social: [
+            {
+                icon: 'Li',
+                href: 'https://www.linkedin.com/in/rohith-sampathi-053ba03b/'
+            },
+            {
+                icon: 'Tw',
+                href: 'https://twitter.com/Rohith_Sampathi'
+            }
+        ]
+    },
+    {
+        firstName: 'D. Lakshmi Krishna',
+        lastName: 'Bhushan',
+        designation: 'Marketing Strategist',
+        details: 'The master ofstrategy at Montaigne. Making the right moves-in chess and in marketing is what makes him the right fit for the role. Additionally, he eats a lot of chicken and loves drinking a bottle of Tang everyday.',
+        social: [
+            {
+                icon: 'Li',
+                href: 'https://www.linkedin.com/in/lakshmi-krishna-7034617a'
+            },
+            {
+                icon: 'Tw',
+                href: 'https://twitter.com/lakshmikrisna?s=09'
+            }
+        ]
+    },
+    {
+        firstName: 'Nisha',
+        lastName: 'Anand',
+        designation: 'Talent Acquisition',
+        details: 'The Maknae of Montaigne. 19, prudent and powerful enough to recruit the right talent for us. Moreover,keeping everybody in harmony and on par is her strength.',
+        social: [
+            {
+                icon: 'Li',
+                href: 'https://www.linkedin.com/in/nisha-anand-273260164/'
+            }
+        ]
+    },
+    {
+        firstName: 'Ahmed',
+        lastName: 'Aleemuddin',
+        designation: 'Lead Developer',
+        details: 'At Montaigne, a motivated Ahmed develops and manages designs and functionalities of websites and servers with his inner creativity and multitasking to create something out-of-the-box! The early bird,always sits on the bean bag,watches Mr.Bean and loves binging comedy shows.',
+        social: [
+            {
+                icon: 'Li',
+                href: 'https://www.linkedin.com/in/ahmed-aleemuddin-2b5392117'
+            }
+        ]
+    },
+    {
+        firstName: 'Narendra',
+        lastName: 'Kumawat',
+        designation: 'Product Specialist',
+        details: 'The tech savy geek who fits in perfectly despite being the calmest among the lot. Pro-active and focused, he strives to perform better and surpasses his own benchmarks.',
+        social: [
+            {
+                icon: 'Github',
+                href: 'https://github.com/narendra-kumawats'
+            },
+            {
+                icon: 'Li',
+                href: 'https://www.linkedin.com/in/narendra-kumawat-a1bb95100/'
+            }
+        ]
+    },
+    {
         firstName: 'Ratnasambhav',
         lastName: 'Priyadarshi',
-        designation: 'Developer',
-        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptate, impedit, repudiandae, accusamus officiis officia unde error necessitatibus beatae dignissimos temporibus laudantium tempore autem reprehenderit ipsa nisi labore expedita mollitia?',
+        designation: 'Product Specialist',
+        details: 'Innovates, creates and perfects every product that Montaigne comes across. A gamer after  work, a music person and reads books once a while.',
         social: [
             {
-                icon: 'github',
-                href: '#'
+                icon: 'Github',
+                href: 'https://github.com/Ratnasambhavs'
             },
             {
-                icon: 'linkedin',
-                href: '#'
+                icon: 'Li',
+                href: 'https://www.linkedin.com/in/ratnasambhav-priyadarshi-496b77108/'
             },
             {
-                icon: 'twitter',
-                href: '#'
-            },
-            {
-                icon: 'instagram',
-                href: '#'
+                icon: 'Tw',
+                href: 'https://twitter.com/ratnasambhav732'
             }
         ]
     },
     {
-        firstName: 'Jhon',
-        lastName: 'Wick',
-        designation: 'Developer',
-        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptate, impedit, repudiandae, accusamus officiis officia unde error necessitatibus beatae dignissimos temporibus laudantium tempore autem reprehenderit ipsa nisi labore expedita mollitia?',
+        firstName: 'Vamsi',
+        lastName: 'Konduri',
+        designation: 'Creative Designer',
+        details: 'A one-of-a-kind creative designer who makes eye-catching visuals of our ideas.He loves clicking and posing for photographs,is a foodie and competes with himself the most.',
         social: [
             {
-                icon: 'github',
-                href: '#'
+                icon: 'Li',
+                href: 'https://www.linkedin.com/in/vamsi-konduri-02b94815a/'
             },
             {
-                icon: 'linkedin',
-                href: '#'
+                icon: 'Tw',
+                href: 'https://twitter.com/vamsikrissh'
             },
             {
-                icon: 'twitter',
-                href: '#'
-            },
-            {
-                icon: 'instagram',
-                href: '#'
-            }
-        ]
-    },
-    {
-        firstName: 'Jhon',
-        lastName: 'HHHJJJJKKKK',
-        designation: 'ASDFGH',
-        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptate, impedit, repudiandae, accusamus officiis officia unde error necessitatibus beatae dignissimos temporibus laudantium tempore autem reprehenderit ipsa nisi labore expedita mollitia?',
-        social: [
-            {
-                icon: 'github',
-                href: '#'
-            },
-            {
-                icon: 'linkedin',
-                href: '#'
-            },
-            {
-                icon: 'twitter',
-                href: '#'
-            },
-            {
-                icon: 'instagram',
-                href: '#'
-            }
-        ]
-    },
-    {
-        firstName: 'Jhon',
-        lastName: 'HHHJJJJKKKK',
-        designation: 'ASDFGH',
-        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptate, impedit, repudiandae, accusamus officiis officia unde error necessitatibus beatae dignissimos temporibus laudantium tempore autem reprehenderit ipsa nisi labore expedita mollitia?',
-        social: [
-            {
-                icon: 'github',
-                href: '#'
-            },
-            {
-                icon: 'linkedin',
-                href: '#'
-            },
-            {
-                icon: 'twitter',
-                href: '#'
-            },
-            {
-                icon: 'instagram',
-                href: '#'
-            }
-        ]
-    },
-    {
-        firstName: 'Jhon',
-        lastName: 'HHHJJJJKKKK',
-        designation: 'ASDFGH',
-        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptate, impedit, repudiandae, accusamus officiis officia unde error necessitatibus beatae dignissimos temporibus laudantium tempore autem reprehenderit ipsa nisi labore expedita mollitia?',
-        social: [
-            {
-                icon: 'github',
-                href: '#'
-            },
-            {
-                icon: 'linkedin',
-                href: '#'
-            },
-            {
-                icon: 'twitter',
-                href: '#'
-            },
-            {
-                icon: 'instagram',
-                href: '#'
-            }
-        ]
-    },
-    {
-        firstName: 'Jhon',
-        lastName: 'HHHJJJJKKKK',
-        designation: 'ASDFGH',
-        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptate, impedit, repudiandae, accusamus officiis officia unde error necessitatibus beatae dignissimos temporibus laudantium tempore autem reprehenderit ipsa nisi labore expedita mollitia?',
-        social: [
-            {
-                icon: 'github',
-                href: '#'
-            },
-            {
-                icon: 'linkedin',
-                href: '#'
-            },
-            {
-                icon: 'twitter',
-                href: '#'
-            },
-            {
-                icon: 'instagram',
-                href: '#'
-            }
-        ]
-    },
-    {
-        firstName: 'Jhon',
-        lastName: 'HHHJJJJKKKK',
-        designation: 'ASDFGH',
-        details: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea voluptate, impedit, repudiandae, accusamus officiis officia unde error necessitatibus beatae dignissimos temporibus laudantium tempore autem reprehenderit ipsa nisi labore expedita mollitia?',
-        social: [
-            {
-                icon: 'github',
-                href: '#'
-            },
-            {
-                icon: 'linkedin',
-                href: '#'
-            },
-            {
-                icon: 'twitter',
-                href: '#'
-            },
-            {
-                icon: 'instagram',
-                href: '#'
+                icon: 'Fb',
+                href: 'https://www.facebook.com/krisshfans'
             }
         ]
     }
@@ -175,11 +131,16 @@ function changeMember(image, index) {
     const body = $('.team--info-body');
     body.addClass('fadeOutDown');
     const memberDetail = memberDetails[index];
+    let socialLinks = '';
+    memberDetail.social.map(link => {
+        socialLinks += `<a href='${link.href}'><div class='${link.icon}'></div></a>`
+    });
     setTimeout(() => {
         $('.first-name').html(memberDetail.firstName);
         $('.last-name').html(memberDetail.lastName);
         $('.designation').html(memberDetail.designation);
         $('.details').html(memberDetail.details);
+        $('.member-social').html(socialLinks);
         body.removeClass('fadeOutDown');
         body.addClass('fadeInDown');
     }, 500);
