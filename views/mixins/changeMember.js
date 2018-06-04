c73d975a4fe718bc1126476027b39651de36e17f
Ratnasambhav Priyadarshi <ratnasambhav@montaigne.co>
2018-06-04 13:15:27 +0530

    Fixed content errors

--- views/mixins/changeMember.js
+++ views/mixins/changeMember.js
@@ -3,7 +3,7 @@ const memberDetails = [
         firstName: 'Rohith',
         lastName: 'Sampathi',
         designation: 'CEO & Founder',
-        details: 'Rohith Sampathi is a management consultant, free-lance writer, business marketer and an automotive enthusiast. During his journey with market intelligence industry, he was extensively working on research modeling for animal nutrition, automotive and machinery industries. Further, having worked as the Marketing Lead for a market research firm in the highly competitive paid marketing arena, his expertise in marketing for a diverse user-base is notable.',
+        details: 'Rohith Sampathi is a management consultant, free-lance writer, business marketer and an automotive enthusiast. During his journey with market intelligence industry, he was extensively working on research modelling for animal nutrition, automotive and machinery industries. Further, having worked as the Marketing Lead for a market research firm in the highly competitive paid marketing arena, his expertise in marketing for a diverse user-base is notable.',
         social: [
             {
                 icon: 'Li',
@@ -19,7 +19,7 @@ const memberDetails = [
         firstName: 'Ahmed',
         lastName: 'Aleemuddin',
         designation: 'Lead Developer',
-        details: 'At Montaigne, a motivated Ahmed develops and manages designs and functionalities of websites and servers with his inner creativity and multitasking to create something out-of-the-box! The early bird,always sits on the bean bag,watches Mr.Bean and loves binging comedy shows.',
+        details: 'At Montaigne, a motivated Ahmed develops and manages designs and functionalities of websites and servers with his inner creativity and multitasking to create something out-of-the-box! The early bird, always sits on the bean bag, watches Mr.Bean and loves binging comedy shows.',
         social: [
             {
                 icon: 'Li',
@@ -31,7 +31,7 @@ const memberDetails = [
         firstName: 'Ananya',
         lastName: 'Chandra',
         designation: 'Content Engineer',
-        details: 'A get-it-done policy individual running forward in life to learn and grow more. By the way,she binge watches shows, sings and looks for excellent buffet places around.',
+        details: 'A get-it-done policy individual running forward in life to learn and grow more. By the way, she binge watches shows, sings and looks for excellent buffet places around.',
         social: [
             {
                 icon: 'Li',
@@ -43,7 +43,7 @@ const memberDetails = [
         firstName: 'Narendra',
         lastName: 'Kumawat',
         designation: 'Product Specialist',
-        details: 'The tech savy geek who fits in perfectly despite being the calmest among the lot. Pro-active and focused, he strives to perform better and surpasses his own benchmarks.',
+        details: 'The tech savvy geek who fits in perfectly despite being the calmest among the lot. Pro-active and focused, he strives to perform better and surpasses his own benchmarks.',
         social: [
             // {
             //     icon: 'Github',
@@ -59,7 +59,7 @@ const memberDetails = [
         firstName: 'D. Lakshmi Krishna',
         lastName: 'Bhushan',
         designation: 'Marketing Strategist',
-        details: 'The master ofstrategy at Montaigne. Making the right moves-in chess and in marketing is what makes him the right fit for the role. Additionally, he eats a lot of chicken and loves drinking a bottle of Tang everyday.',
+        details: 'The master of strategy at Montaigne. Making the right moves-in chess and in marketing is what makes him the right fit for the role. Additionally, he eats a lot of chicken and loves drinking a bottle of Tang every day.',
         social: [
             {
                 icon: 'Li',
@@ -75,7 +75,7 @@ const memberDetails = [
         firstName: 'Nisha',
         lastName: 'Anand',
         designation: 'Talent Acquisition',
-        details: 'The Maknae of Montaigne. 19, prudent and powerful enough to recruit the right talent for us. Moreover,keeping everybody in harmony and on par is her strength.',
+        details: 'The Maknae of Montaigne. 19, prudent and powerful enough to recruit the right talent for us. Moreover, keeping everybody in harmony and on par is her strength.',
         social: [
             {
                 icon: 'Li',
@@ -87,7 +87,7 @@ const memberDetails = [
         firstName: 'Ratnasambhav',
         lastName: 'Priyadarshi',
         designation: 'Product Specialist',
-        details: 'Innovates, creates and perfects every product that Montaigne comes across. A gamer after  work, a music person and reads books once a while.',
+        details: 'Innovates, creates and perfects every product that Montaigne comes across. A gamer after  work,a music person and reads books once a while. The bio might look a little too normal but you’ve gotta be here to witness his specialities.',
         social: [
             // {
             //     icon: 'Github',
@@ -107,7 +107,7 @@ const memberDetails = [
         firstName: 'Vamsi',
         lastName: 'Konduri',
         designation: 'Creative Designer',
-        details: 'A one-of-a-kind creative designer who makes eye-catching visuals of our ideas.He loves clicking and posing for photographs,is a foodie and competes with himself the most.',
+        details: 'A one-of-a-kind creative designer who makes eye-catching visuals of our ideas. He loves clicking and posing for photographs,is a foodie and competes with himself the most. Mark my words, The Vamsi-Bhushan duo are on your nerves all day.',
         social: [
             {
                 icon: 'Li',
